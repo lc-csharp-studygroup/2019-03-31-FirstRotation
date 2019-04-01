5c0cec279315154699086dad121680478b6ddb5f
CailieAC <cailiecarlile@gmail.com>
2019-03-31 19:00:29 -0500

    CAC set properties

--- CoderGirl-HighSchool/HighSchool/Program.cs
+++ CoderGirl-HighSchool/HighSchool/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HighSchool
 {
     public static class Program
     {
+        //contains a Student class. You must track the student’s first name, last name, grade (1 - 12), 
+        //and current GPA (1-4). Store a list of 10 or more fake students in a list. 
+        //Then, use LINQ to print a list of students with a GPA of 3 or higher that are in the 9th - 12th grade. 
+        //The list should output their first name, last name, grade and GPA. 
+        //It should be sorted by highest grade level, then highest GPA, then last name, then first name.
+
+        /*1. The Student class should have a static method called "StarStudents" which takes a Student list 
+         * and returns a Student list that
+         * - contains only the students in grades 9-12
+         * - is sorted by highest grade, then highest GPA, then last name, then first name. (edited)
+         * 2. The Student class GPA property should have a min value of 0 and max value of 4. 
+         * If a user of the class tries to set the value of the GPA below/above the min/max, 
+         * it will be set at the min/max instead.
+         * 3. The Student Grade has a min of 1 and a max of 12. If a user of the class tries to set 
+         * the value of the Grade below/above the min/max, it will be set at the min/max instead.
+         * The rest of the instructions about printing etc. aren't tested and therefore aren't 
+         * part of the requirements. Its probably a good idea to do so for testing purposes.
+         */
+
         public static void Main()
         {
             Console.ReadLine();
+            Student jill = new Student();
+            jill.GPA = 5.0;
+            Student billy = new Student();
+            billy.GPA = 2.0;
+
+            List<Student> studentList = new List<Student>
+            {
+                new Student {FirstName = "John", LastName = "Brown", Grade = 3, GPA = 4.0 },
+                new Student {FirstName = "Christine", LastName = "Brown", Grade = 10, GPA = 3.5 },
+                new Student {FirstName = "Sarah", LastName = "Striker", Grade = 6, GPA = 0.5 },
+                new Student {FirstName = "Jessica", LastName = "Reynolds", Grade = 12, GPA = 3.6 },
+                new Student {FirstName = "Wyatt", LastName = "Mayhan", Grade = 1, GPA = 2.5 },
+                new Student {FirstName = "Xander", LastName = "Carrington", Grade = 9, GPA = 1.9 },
+                new Student {FirstName = "Riley", LastName = "Vu", Grade = 5, GPA = 2.8 },
+                new Student {FirstName = "Jessie", LastName = "Chin", Grade = 2, GPA = 3.2 },
+                new Student {FirstName = "Maria", LastName = "Espinoza", Grade = 8, GPA = 2.0 },
+                new Student {FirstName = "Dean", LastName = "Winchester", Grade = 4, GPA = 4.0 },
+                new Student {FirstName = "Eric", LastName = "Prince", Grade = 7, GPA = 1.7 },
+                new Student {FirstName = "Elizabeth", LastName = "Jones", Grade = 11, GPA = 3.9 },
+                new Student { FirstName = "Alan", LastName = "Z", GPA = 3.0, Grade = 9 },
+                new Student { FirstName = "Barb", LastName = "Y", GPA = 3.1, Grade = 9 },
+                new Student { FirstName = "Cassie", LastName = "X", GPA = 3.2, Grade = 9 },
+                new Student { FirstName = "Derek", LastName = "W", GPA = 3.3, Grade = 9 },
+                new Student { FirstName = "Elen", LastName = "V", GPA = 3.3, Grade = 9 },
+                new Student { FirstName = "Fola", LastName = "U", GPA = 3.3, Grade = 9 },
+                new Student { FirstName = "Ginny", LastName = "T", GPA = 3.0, Grade = 12 },
+                new Student { FirstName = "Hannah", LastName = "S", GPA = 3.1, Grade = 12 },
+                new Student { FirstName = "Ian", LastName = "R", GPA = 3.2, Grade = 12 },
+                new Student { FirstName = "Jolynn", LastName = "Q", GPA = 3.3, Grade = 12 }
+            };
         }
     }
 }
--- CoderGirl-HighSchool/HighSchool/Student.cs
+++ CoderGirl-HighSchool/HighSchool/Student.cs
@@ -5,8 +5,52 @@ namespace HighSchool
 {
     public class Student
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private int _grade;
+        public int Grade
+        {
+            get { return _grade; }
+            set
+            {
+                if (value < 1)
+                {
+                    _grade = 1;
+                }
+                else if (value > 12)
+                {
+                    _grade = 12;
+                }
+                else
+                    _grade = value;
+            }
+                
+        }
+
+        private double _GPA;
+        public double GPA
+        {
+            get { return _GPA; }
+            set
+            {
+                if (value < 0)
+                {
+                    _GPA = 0;
+                }
+                else if (value > 4)
+                {
+                    _GPA = 4;
+                }
+                else
+                    _GPA = value;
+            }
+        }
+        
         public static List<Student> StarStudents(List<Student> students)
         {
+
         }
+        
     }
 }
